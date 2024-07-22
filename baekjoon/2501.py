232af622d2d7aeb1d481afd505223e2d3508099b
jcahn <ajc227ung@gmail.com>
2024-07-22 16:23:25 +0900

    're'

--- baekjoon/2501.py
+++ baekjoon/2501.py
@@ -1,16 +1,13 @@
 N, K = map(int, input().split(' '))
 
-cnt = 0
-for _ in range(1,N+1) :
-    if N % _ == 0 :
-        cnt += 1
-        result = _
-    else :
-        pass
-    if cnt == K :
-        break
-    
-if cnt == K :
-    print(result)
-else :
+rl = []
+for i in range(1,int(N ** (1/2)) + 1) :
+    if N % i == 0 :
+        rl.append(i)
+        if i < N//i :
+            rl.append(N // i) 
+rl.sort()
+try :
+    print(rl[K-1])
+except :
     print(0)
